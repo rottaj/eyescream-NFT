7331737da59a5476dc4f8d1554dddf2246add0a2
rottaj <jackrotta12@gmail.com>
2021-10-23 13:13:22 -0400

    updated mintcard

--- frontend/src/Components/MintCard.tsx
+++ frontend/src/Components/MintCard.tsx
@@ -1,32 +1,53 @@
 import React from 'react';
 import EyeScreamPreview from "../images/eyescreamscoop.jpg";
 
-export default function MintCard() {
-    return (
-        <div className="mint-card">
-            <div className="mint-card-title">
-                <h3>Enter the amount of Eyescream you'd like to buy</h3>
-            </div>
-            <div className="mint-card-eyescream">
-                <img src={EyeScreamPreview} className="mint-img"></img>
-                <div className="mint-card-eyescream-info">
-                    <h3 className="current-price-h3">Current Price</h3>
-                    <h1>0.08 ETH Each</h1>
-                    <h3>10,101/10,101 remain</h3>
+ class MintCard extends React.Component {
+
+    state = {
+        coins: 1,
+        amountEth: 0.08,
+    }
+
+    onChangeCalculateTotal = (e: React.ChangeEvent<HTMLInputElement>) => {
+        console.log(e)
+        let tokenNum = parseInt(e.target.value) || 0;
+        if (tokenNum > 5) { tokenNum = 5 }
+        let amount = tokenNum * 0.08 
+        //let amount = parseInt(e.target.value) * 0.08;
+        this.setState({
+            coins: tokenNum,
+            amountEth: amount
+        })
+    }
+     render() {
+        return (
+            <div className="mint-card">
+                <div className="mint-card-title">
+                    <h3>Enter the amount of Eyescream you'd like to buy</h3>
                 </div>
-            </div>
+                <div className="mint-card-eyescream">
+                    <img src={EyeScreamPreview} className="mint-img"></img>
+                    <div className="mint-card-eyescream-info">
+                        <h3 className="current-price-h3">Current Price</h3>
+                        <h1>0.08 ETH Each</h1>
+                        <h3>10,101/10,101 remain</h3>
+                    </div>
+                </div>
 
-            <form className="mint-form">
-                <input className="mint-form-token-amount" type="text" id="token-amount" name="token-amount" value="1" placeholder="Enter number of Eyescreams you want."/>
-            </form>
-            <hr></hr>
-            <div className="mint-card-total">
-                <h3 className="total-h3">Total</h3>
-                <h1 className="mint-card-total-price-h1">0.08 ETH</h1>
-            </div>
-            <hr></hr>
-            <button className="mint-card-button-buy">Buy now!</button>
+                <form className="mint-form">
+                    <input className="mint-form-token-amount" type="text" id="token-amount" name="token-amount" placeholder="Enter number of Eyescreams you want." value={this.state.coins} onChange={this.onChangeCalculateTotal}/>
+                </form>
+                <hr></hr>
+                <div className="mint-card-total">
+                    <h3 className="total-h3">Total</h3>
+                    <h1 className="mint-card-total-price-h1">{this.state.amountEth} ETH</h1>
+                </div>
+                <hr></hr>
+                <button className="mint-card-button-buy">Buy now!</button>
 
-        </div>
-    )
-}
+            </div>
+        )
+    }
+ }
+
+ export default MintCard

8220d92f9bc68771d46c6746c8c5943c5c929172
rottaj <jackrotta12@gmail.com>
2021-11-10 12:58:44 -0500

    added getTokenURI test.

--- test/sample-test.js
+++ test/sample-test.js
@@ -55,22 +55,24 @@ describe("Get Total Supply", function () {
 })
 
 describe("GetTokenURI", function () {
-  it("Should return token URI based on _tokenId", async function (_tokenId) {
+  it("Should return token URI based on _tokenId", async function (done) {
     const Eyescream = await ethers.getContractFactory("Eyescream");
     const eyescream = await Eyescream.deploy();
     await eyescream.deployed();
+    let baseTokenURI = "https://ipfs.io/ipfs/QmXCRjMJ847xsSA5RMyvDNbYQnr3sQLB1icA6NxHnjhY4y/"
 
+    txnTwo = await eyescream.setBaseTokenURI(baseTokenURI);
+    await txnTwo.wait();
+    console.log("BASE TOKEN URI SET")
     let quantity = 2
     txn = await eyescream.mint(quantity, { // mints 2 tokens
         value: ethers.utils.parseEther((0.08 * quantity).toString())
     });
     await txn.wait();
 
-    console.log("Minted");
-
     const tokenURI = await eyescream.tokenURI(1);
-    await tokenURI
-
+    await tokenURI;
+    //done();     
     console.log("Test TOKEN URI", tokenURI)
   })
 })

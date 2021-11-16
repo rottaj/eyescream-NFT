b4cdec9f9e2148bef2c991b2ab2f6dc80caa8226
rottaj <jackrotta12@gmail.com>
2021-11-15 21:38:25 -0500

    added team page, fonts, another shit

--- frontend/src/Components/TeamPage.tsx
+++ frontend/src/Components/TeamPage.tsx
@@ -1,17 +1,20 @@
 import React from 'react';
-import DripBackgroundBanner from "../images/DripBackgroundBanner.png";
 import PhyllisToken from "../images/my_fucking_mayc.png";
 import AhawkToken from "../images/ahawk_nft.png";
+import EyescreamScoop from "../images/eyescreamscoop.jpg";
 import "./TeamPage.css";
 
 export default class TeamPage extends React.Component {
     render() {
         return (
             <div className="TeamPage-Main">
-                <h1>Signature Flavors</h1>
+                <h1 className="h1-signature-flavors">Signature Flavors</h1>
                 <div className="Team-Container">
                     <img className="Phyllis-token" src={PhyllisToken} />
                     <img className="Ahawk-token" src={AhawkToken} />
+                    <img className="eyescream-token" src={EyescreamScoop} />
+                    <img className="eyescream-token" src={EyescreamScoop} />
+                    <img className="eyescream-token" src={EyescreamScoop} />
                 </div>
             </div>
         )

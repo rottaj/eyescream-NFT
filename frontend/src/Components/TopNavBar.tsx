b0b18dbf4c1662dcf6b25d570e58b4caa5178bcd
rottaj <jackrotta12@gmail.com>
2021-11-15 22:01:56 -0500

    updated top navbar

--- frontend/src/Components/TopNavBar.tsx
+++ frontend/src/Components/TopNavBar.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
-
+import "./TopNavBar.css";
 
 export default class TopNavBar extends React.Component {
     render() {
         return (
             <div className="top-nav-bar">
+                <h2 className="top-nav-bar-h2">The Eyescream Social</h2>
                 <button className="connect-wallet-button" >Connect Wallet</button>
             </div>
         )

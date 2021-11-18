f1fb41911ab012b41c2cb85846a21be95d3cfd7a
rottaj <jackrotta12@gmail.com>
2021-11-17 22:57:15 -0500

    added dripity dropity

--- frontend/src/Components/CountDown.tsx
+++ frontend/src/Components/CountDown.tsx
@@ -60,37 +60,64 @@ class CountDown extends React.Component<Props, State, Interval> {
     render() {
         return (
             <div className="count-down-panel">
+                <svg xmlns="http://www.w3.org/2000/svg" version="1.1"  className="liquid-drop">
+                    <defs>
+                        <filter id="liquid">
+                        <feGaussianBlur in="SourceGraphic" stdDeviation="10" result="blur" />
+                        <feColorMatrix in="blur" mode="matrix" values="1 0 0 0 0  0 1 0 0 0  0 0 1 0 0  0 0 0 18 -7" result="liquid" />
+                        </filter>
+                    </defs>
+                </svg>
                 <div className="count-down-card">
                 <div className="count-down-card-time-container">
+                    <div className="count-down-container-days">
                     <div className="count-down-panel-days">
                         <h1>{this.state.days}</h1> <br></br>
                         <h4> Days</h4>
-                        <div className="drops">
-                            <div className="drop1"></div>
-                            <div className="drop2"></div>  
-                        </div>
 
-                        <svg xmlns="http://www.w3.org/2000/svg" version="1.1"  className="liquid-drop">
-                            <defs>
-                                <filter id="liquid">
-                                <feGaussianBlur in="SourceGraphic" stdDeviation="10" result="blur" />
-                                <feColorMatrix in="blur" mode="matrix" values="1 0 0 0 0  0 1 0 0 0  0 0 1 0 0  0 0 0 18 -7" result="liquid" />
-                                </filter>
-                            </defs>
-                        </svg>
                     </div>
+                    <div className="drops">
+                        <div className="drop1"></div>
+                        <div className="drop2"></div>  
+                    </div>
+
+                    </div>
+
+                    <div className="count-down-container-hours">
                     <div className="count-down-panel-hours">
                         <h1>{this.state.hours}</h1>
                         <h4> Hours</h4>
                     </div>
+                    <div className="drops">
+                        <div className="drop1"></div>
+                        <div className="drop2"></div>  
+                    </div>
+                    </div>
+
+
+                    <div className="count-down-container-minutes">
                     <div className="count-down-panel-minutes">
                         <h1>{this.state.minutes}</h1>
                         <h4>Minutes</h4>
                     </div>
+                    <div className="drops">
+                        <div className="drop1"></div>
+                        <div className="drop2"></div>  
+                    </div>
+                    </div>
+
+                    <div className="count-down-container-seconds">
                     <div className="count-down-panel-seconds">
                         <h1>{this.state.seconds}</h1>
                         <h4>Seconds</h4>
                     </div>
+                    <div className="drops">
+                        <div className="drop1"></div>
+                        <div className="drop2"></div>  
+                    </div>
+                    </div>
+
+
 
 
 

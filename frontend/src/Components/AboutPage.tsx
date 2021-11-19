f6b8787604f774497f6a21949fcb125e62bea2f8
rottaj <jackrotta12@gmail.com>
2021-11-19 00:47:57 -0500

    downgraded npm.. added some css

--- frontend/src/Components/AboutPage.tsx
+++ frontend/src/Components/AboutPage.tsx
@@ -10,14 +10,14 @@ export default class AboutPage extends React.Component {
                         <h2 className="AboutCard-h2">EYESCREAM</h2>
                         <h3 className="AboutCard-h5">ALLOWS YOU TO COLLABORATE AND CONNECT WITH OTHER CREATORS.</h3>
                         <h4 className="AboutCard-h4">
-                        EACH TOKEN PROVIDES ACCESS TO OUR EXCLUSIVE COMMUNITY WHERE MEMBERS CAN COLLABORATE, CREATE, LEARN, NETWORK, AND
+                        EACH TOKEN PROVIDES ACCESS TO OUR COMMUNITY WHERE MEMBERS CAN COLLABORATE, CREATE, LEARN, NETWORK, AND
                         PIONEER WEB3.
                         <br></br><br></br>
-                        OUR MISSION IS TO BUILDAN ENVIRONMENT WHERE PASSIONATE AND CREATIVE PEOPLE CAN MAKE MEANINGFUL CONTRIBUTIONS TO OUR PLANET.
+                        OUR MISSION IS TO BUILD AN ENVIRONMENT WHERE PASSIONATE AND CREATIVE PEOPLE CAN MAKE MEANINGFUL CONTRIBUTIONS TO OUR PLANET.
                         <br></br><br></br>
-                        WE BELIEVE THAT MAKING INTRODUCING FRESH PERSPECTIVES AND NEW TECHNIQUES ALLOWS PEOPLE TO EVOLVE AND GROW AS A COMMUNITY.
+                        WE BELIEVE THAT INTRODUCING FRESH PERSPECTIVES AND NEW TECHNIQUES ALLOWS PEOPLE TO EVOLVE AND GROW AS A COMMUNITY.
                         <br></br><br></br>
-                        IT TAKES TIME TO BUILD SOMETHING THAT'S WORTH BUILDING.
+                        IT TAKES TIME TO BUILD SOMETHING THAT'S WORTH BUILDING. WE DONT MAKE EMPTY PROMISES, WE DELIVER.
                         </h4>
                     </div>
                 </div>
